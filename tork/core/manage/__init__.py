e4907b0e222ffe207f5e7826fc6c2e32bb907c41
Van Nguyen <thegoleffect@gmail.com>
2009-12-09 22:59:03 -0500

    adding my current set of additions

---
+++ tork/core/manage/__init__.py
@@ -0,0 +1,29 @@
+import sys, os
+from optparse import OptionParser
+import tork
+
+options_list = [
+  [["-i", "--init"], {'dest':"init_flag", "default":False, "help":"Initialize a Tornado Web application with MVC folders"}],
+  [["-t", "--test"], {'dest':"test_flag", "default":False, "help":"Run all tests for local application"}]
+]
+commands_list = ["init", "start", "stop", "restart", "test"]
+
+def run(argv=None):
+  parser = OptionParser(usage="%s subcommand [options] [args]",
+                        version=tork.get_version())
+  for option in options_list:
+    parser.add_option(*option[0], **option[1])
+  
+  try:
+    options, args = parser.parse_args(argv)
+  except Exception, e:
+    pass # Ignore OptionError
+  
+  if args[0] in commands_list:
+    print args[0], "called"
+  else:
+    print "Usage:", parser.usage %(os.path.basename(sys.argv[0]))
+    print ""
+    print "Type 'tork help' for usage information"
+  
+  
